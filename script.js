ae0d4b20bf584bf1e05df7bc42a8667cc540e2f0
dqcec <52030830+dqcec@users.noreply.github.com>
2019-08-30 02:00:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ $(function(){
    var n=0;
   $('.textChange').click(function(){
      n=n+1;
+     if(n<11){
 $('.changeText').text(n+'回クリックされました');
+     }else{
+ $('.changeText').text('そんなにクリックしても意味ないよ？笑');
+     };
   });
 });
