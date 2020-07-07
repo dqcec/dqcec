93caa675714ac3a822af1f35b23fbff35d89b056
dqcec <52030830+dqcec@users.noreply.github.com>
2020-07-07 18:34:49 +0900

    Update PHP.php

--- PHP.php
+++ PHP.php
@@ -6,12 +6,13 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>PHP</title>
+  <title>PHPfile</title>
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
   <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
-
+<h1>PHP</h1>
+  <a href='https://dqcec.github.io/dqcec/index.html'>自作webページ</a>
 </body>
 </html>
